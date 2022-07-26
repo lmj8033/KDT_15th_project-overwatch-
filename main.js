bcb1974a23447dd8dceadc7ef9006d5f9c9fb145
lmj8033 <lmj8033@naver.com>
2022-07-26 16:23:27 +0900

    overwatchproject

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 
+const rightMenu = document.querySelector("#user .right_menu");
+const snbBtn = document.querySelector("#user .btn_menu");
+
+snbBtn.addEventListener("click",function(){
+    if(rightMenu.classList.contains("hide")) {
+        rightMenu.classList.remove("hide");
+    }else {
+        rightMenu.classList.add("hide");
+    }
+})
+
+const leftMenu = document.querySelector("#main .left_menu");
+const mainBtn = document.querySelector("#main .left_btn_menu");
+
+mainBtn.addEventListener("click", function () {
+    if(leftMenu.classList.contains("hidden")) {
+        leftMenu.classList.remove("hidden");
+    }else {
+        leftMenu.classList.add("hidden");
+    }
+})
